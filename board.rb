d1dc70c85ac9772bdd0626a1c95ed14a07afa133
JennyJ2012 && Gilansalehi <jennyj2012@gmail.com && gilansalehi@gmail.com>
2015-12-04 11:59:38 -0500

    Create Tile#reveal, Board#populate, Board#assign_bomb.

--- board.rb
+++ board.rb
@@ -1,5 +1,26 @@
 class Board
   GRID_SIZE = 9
-  def initialize
+
+  attr_reader :num_bombs, :grid
+  def initialize(num_bombs)
+    @grid = Array.new(GRID_SIZE) {Array.new(GRID_SIZE)}
+    @num_bombs = num_bombs
+  end
+
+  def populate
+    grid.each_with_index do |row , i|
+      row.each_with_index do |cell, j|
+        cell = Tile.new([i,j], bomb = false)
+      end
+    end
+    assign_bomb
+
+  end
+
+  def assign_bomb
+    x_rand = rand(GRID_SIZE)
+    y_rand = rand(GRID_SIZE)
+
+    num_bombs.times {self.grid[x_rand, y_rand].set_bomb}
   end
 end
--- tile.rb
+++ tile.rb
@@ -1,15 +1,30 @@
 class Tile
 
-  def initialize(board, bomb?)
+  SYMBOLS = [:., :F, :*]
+
+  attr_reader :value, :bomb, :status
+
+  def initialize(pos, bomb = false, status = :hidden)
+    @pos = # [x, y]
+    @bomb = bomb # a boolean, default to false (true if contains a bomb)
+    @status = status # hidden or revealed or Flagged
     @value = neighbor_bomb_count
   end
 
   def reveal
+    # if my value is zero, call reveal on all my neighbors too
+    # maybe delegate this responsibility to the Board class
+    self.status = :revealed
+    bomb ? :* : value
   end
 
   def neighbors
   end
 
   def neighbor_bomb_count
+  end
+
+  def set_bomb
+    self.bomb = true
   end
 end
