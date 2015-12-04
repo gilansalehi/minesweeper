3226f5a26c426ca826b9c6491881f0f02ce5a0e7
JennyJ2012 && Gilansalehi <jennyj2012@gmail.com && gilansalehi@gmail.com>
2015-12-04 11:23:21 -0500

    Initialize Tile and Board class, write basic method outlines.

--- board.rb
+++ board.rb
@@ -1,2 +1,5 @@
 class Board
+  GRID_SIZE = 9
+  def initialize
+  end
 end
--- tile.rb
+++ tile.rb
@@ -1,2 +1,15 @@
 class Tile
+
+  def initialize(board, bomb?)
+    @value = neighbor_bomb_count
+  end
+
+  def reveal
+  end
+
+  def neighbors
+  end
+
+  def neighbor_bomb_count
+  end
 end
